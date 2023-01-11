15d1f7319d5e6e652729a7f33b7e7dfc7eb3b7ba
javad <javad.feyzabadi@yahoo.com>
2023-01-11 20:01:23 +0330

    modified

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+class Post(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    caption = models.TextField(max_length=2080)
+    is_active = models.BooleanField(default=True)
+    is_public = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'post'
+
+    def __str__(self):
+        return self.title
+
+class PostFile(models.Model):
+    post = models.ForeignKey(to=Post,on_delete=models.CASCADE)
+    file = models.FileField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'post file'
+        verbose_name_plural = 'posts file'
+        db_table = 'post file'
+
+    def __str__(self):
+        return self.post
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from  . models import Post
+from . serializers import PostSerializer
+class PostView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request,post_pk):
+        try:
+            post = Post.objects.get(pk=post_pk,user = request.user)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user = request.user)
+        #     return HttpResponse(serializer.data)
+        # return HttpResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)   
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class PostListView(APIView):
+    def get(self,request):
+        post = Post.objects.filter(is_active = True)
+        serializer = PostSerializer(post,many = True)
+        return Response(serializer.data)
+        
