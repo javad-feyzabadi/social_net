947ffde7363286d4ff1d48b5806670760d6a7da2
javad <javad.feyzabadi@yahoo.com>
2023-01-11 19:59:39 +0330

    admin register

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Post,PostFile
+
+
+class PstFileInlineAdmin(admin.StackedInline):
+    model = PostFile
+    fields = ('file',)
+    extra = 0
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user','title','is_active','is_public')
+    inlines = [PstFileInlineAdmin,]
+
+
