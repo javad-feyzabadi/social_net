0527e8629f3a46df056a13c718374d8992e248ba
javad <javad.feyzabadi@yahoo.com>
2023-01-11 19:58:20 +0330

    create serializers.py

---
+++ posts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from . models import Post,PostFile
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('user','title','caption','is_active','is_public')
+        extra_kwargs = {
+            'user':{'read_only':True}
+        }
