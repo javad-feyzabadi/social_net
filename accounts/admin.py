2dcafe0f4d930da97729a0bc81e8bddf85044251
javad <javad.feyzabadi@yahoo.com>
2023-02-16 20:47:14 +0330

    modefied

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from . models import Profile,Country
 
 
-# admin.site.register(Profile)
-# admin.site.register(Country)
-
-
-
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'phone_number']
@@ -16,16 +11,5 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['name', 'abbr', 'is_active']
-
-# class CountryInlineAdmin(admin.StackedInline):
-#     model = Country
-#     fields = ('name','abbr','is-active')
-#     extra = 0 
-
-# @admin.register(Profile)
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = ('user','phone_number','country','avatar')
-#     inlines = [CountryInlineAdmin]
-
 
 
